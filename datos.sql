2c8ea4af8263eb50d0817f008bd6711e85c03154
Luiiis13 <104355898+Luiiis13@users.noreply.github.com>
2022-11-08 21:11:39 +0100

    Se han eliminado los ficheros sql de la raiz

--- datos.sql
+++
@@ -1,2 +0,0 @@
-﻿insert into rol(rol,descripcion) values ("usuario","rol que permite operaciones con tarjeta ");
-insert into rol(rol,descripcion) values ("administrador","rol que permite todas las gestiones ");
