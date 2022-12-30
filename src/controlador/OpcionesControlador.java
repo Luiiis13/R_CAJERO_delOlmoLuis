0de39d8b9d81014f3ee9ef4a3d8363bd75826a93
Luiiis13 <104355898+Luiiis13@users.noreply.github.com>
2022-12-30 19:50:36 +0100

    Se han eliminado controles innecesarios

--- src/controlador/OpcionesControlador.java
+++ src/controlador/OpcionesControlador.java
@@ -1,13 +1,12 @@
 package controlador;
 
-import vista.OpcionesFrame;
 import vista.OpcionesUsuario;
 
 public class OpcionesControlador {
-private OpcionesFrame opcionesFrame;
+
 private OpcionesUsuario opcionesUsuario;
 public OpcionesControlador() {
-	//this.opcionesFrame=new OpcionesFrame();
+	
 	this.opcionesUsuario=new OpcionesUsuario();
 	}
 }
