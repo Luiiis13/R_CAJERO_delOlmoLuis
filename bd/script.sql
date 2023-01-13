517b042da9b3162cdd841e6e01f809fec0a40092
Luiiis13 <104355898+Luiiis13@users.noreply.github.com>
2023-01-13 19:48:16 +0100

    Se agrega la columna contraseña a la tabla usuarios

--- bd/script.sql
+++ bd/script.sql
@@ -16,7 +16,8 @@ id int AUTO_INCREMENT primary key not null,
 nombre varchar(20) not null,
 edad int not null,
 primer_apellido varchar(20),
-rol varchar(20) not null
+rol varchar(20) not null,
+contraseña varchar(50) null
 );
 
 
