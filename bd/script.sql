d771a7e409d51e047a54c2c0afcb44eaa5c42285
Luiiis13 <104355898+Luiiis13@users.noreply.github.com>
2023-01-15 14:51:32 +0100

    Se agrega la nueva estructura de base de datos

--- bd/script.sql
+++ bd/script.sql
@@ -1,8 +1,9 @@
 
 use progamacion;
 
-drop table if exists  usuario_rol;
 drop table if exists  Movimiento;
+drop table if exists Bonobus;
+drop table if exists telefono;
 drop table if exists  Cuenta;
 drop table if exists  Rol;
 drop table if exists  Usuario;
@@ -14,12 +15,11 @@ drop table if exists  Cajero;
 create table Usuario(
 id int AUTO_INCREMENT primary key not null, 
 nombre varchar(20) not null,
-edad int not null,
+dni char(9) not null,
 primer_apellido varchar(20),
-isAdmin boolean not null,
+es_admin boolean not null,
 contraseña varchar(50) null
 );
-
 
 
 create table Tarjeta(
@@ -28,7 +28,9 @@ numero int unique key not null,
 fecha_expiracion date not null,
 cvv int not null, 
 pin int not null,
-bloqueado boolean not null
+bloqueado boolean not null,
+id_cuenta_asociada int not null,
+foreign key (id_cuenta_asociada) references Cuenta(id) ON DELETE CASCADE
 );
 
 create table Cuenta(
@@ -36,10 +38,8 @@ id int AUTO_INCREMENT primary key not null,
 numero int unique key not null,
 iban varchar(20) unique key not null,
 saldo float not null ,
-tarjeta_asociada int not null,
 id_usuario int not null  ,
-foreign key (id_usuario) references usuario(id),
-foreign key(tarjeta_asociada) REFERENCES tarjeta(id) 
+foreign key (id_usuario) references usuario(id) 
 );
 
 
@@ -47,24 +47,26 @@ create table Movimiento(
 id int auto_increment primary key not null ,
 fecha_del_movimiento date not null,
 tipo varchar(20) not null ,
-cuenta_asociada int not null,
-foreign key(cuenta_asociada) REFERENCES cuenta(id)
+id_tarjeta_asociada int not null,
+foreign key(id_tarjeta_asociada) REFERENCES tarjeta(id) ON DELETE CASCADE
 );
 
 create table Cajero(
 id int auto_increment not null  primary key,
 ubicacion varchar(40) not null
 );
+
 create table Bonobus(
 id int primary key not null auto_increment,
 saldo float not null,
-id_cuenta int not null unique,
-foreign key(id_cuenta) REFERENCES cuenta(id)
+id_tarjeta_asociada int not null ,
+foreign key(id_tarjeta_asociada) REFERENCES tarjeta(id) ON DELETE CASCADE
 );
+
 create table telefono(
 id int primary key not null auto_increment,
 numero int not null,
 saldo float not null,
-id_cuenta int not null unique,
-foreign key(id_cuenta) REFERENCES cuenta(id)
+id_tarjeta_asociada int not null ,
+foreign key(id_tarjeta_asociada) REFERENCES tarjeta(id)  ON DELETE CASCADE
 );
